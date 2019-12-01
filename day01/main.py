c33cc37124a8ed06c7cda441e02b185cbd9a2e59
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-01 10:14:13 -0500

    Day 1, Part 2

--- day01/main.py
+++ day01/main.py
@@ -3,13 +3,26 @@ def fuel_counter_upper(mass):
     return mass // 3 - 2
 
 
+def fuel_included_counter(mass):
+    sum = 0
+    fuel_mass = fuel_counter_upper(mass)
+    while fuel_mass >= 0:
+        sum += fuel_mass
+        fuel_mass = fuel_counter_upper(fuel_mass)
+
+    return sum
+
+
+
 def main():
     with open('./input') as file:
         inputs = file.readlines()
-        sum = 0
+        part_one = part_two = 0
         for line in inputs:
-            sum += fuel_counter_upper(int(line))
+            part_one += fuel_counter_upper(int(line))
+            part_two += fuel_included_counter(int(line))
 
-        return sum
+        return part_one, part_two
 
-print(main())
+print("The answer to part 1 is %d\n"
+      "The answer to part 2 is %d" % main())
