b90d5436f3407720e20f46a3b81aa10fec2c9fea
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-04 09:47:44 -0500

    Day 4, Part 2

--- day04/main.py
+++ day04/main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def meets_criteria(passwd):
     digits = list(map(lambda x: (passwd // (10**x)) % 10,
                       reversed(range(0,6))))
@@ -10,10 +12,17 @@ def meets_criteria(passwd):
 
     return doubled
 
+def meets_part2_criteria(passwd):
+    digits = list(map(lambda x: (passwd // (10**x)) % 10,
+                      reversed(range(0,6))))
+    return 2 in Counter(digits).values()
+
 def main():
-    part1 = sum(meets_criteria(i) for i in range(125730, 579381))
-    return part1
+    passing = [i for i in range(125730, 579381) if meets_criteria(i)]
+    part1 = len(passing)
+    part2 = len([i for i in passing if meets_part2_criteria(i)])
+    return part1, part2
 
 
 print("The answer to part 1 is %d\n"
-      "The answer to part 2 is " % main())
+      "The answer to part 2 is %d" % main())
