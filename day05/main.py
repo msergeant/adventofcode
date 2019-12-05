cb2adbd70982f8587595998e21cd6fae5993fab2
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-05 10:39:20 -0500

    Day 5, Part 2

--- day05/main.py
+++ day05/main.py
@@ -9,7 +9,7 @@ def opcode_2(index, program_list):
     return 4, program_list[target]
 
 def opcode_3(index, program_list):
-    num = int(input("op3: "))
+    num = int(input("1 for part 1; 5 for part 2: "))
     program_list[program_list[index + 1]] = num
     return 2, num
 
@@ -17,6 +17,36 @@ def opcode_4(index, program_list):
     (num, _, _) = parse_mode(index, program_list)
     print("op4", index, program_list[num])
     return 2, program_list[num]
+
+def opcode_5(index, program_list):
+    (tst, addr, _) = parse_mode(index, program_list)
+    if program_list[tst]:
+        return program_list[addr] - index, -1
+    else:
+        return 3, -1
+
+def opcode_6(index, program_list):
+    (tst, addr, _) = parse_mode(index, program_list)
+    if program_list[tst]:
+        return 3, -1
+    else:
+        return program_list[addr] - index, -1
+
+def opcode_7(index, program_list):
+    (left, right, target) = parse_mode(index, program_list)
+    if program_list[left] < program_list[right]:
+        program_list[target] = 1
+    else:
+        program_list[target] = 0
+    return 4, -1
+
+def opcode_8(index, program_list):
+    (left, right, target) = parse_mode(index, program_list)
+    if program_list[left] == program_list[right]:
+        program_list[target] = 1
+    else:
+        program_list[target] = 0
+    return 4, -1
 
 def parse_mode(index, program_list):
     command = program_list[index]
@@ -35,21 +65,24 @@ def main():
         opcode_2,
         opcode_3,
         opcode_4,
+        opcode_5,
+        opcode_6,
+        opcode_7,
+        opcode_8,
     ]
 
     opcode = program_list[0]
-    part1 = -1
+    answer = -1
     while index < len(program_list) and opcode != 99:
         offset, result = ops[opcode % 10](index, program_list)
         index += offset
         if(opcode % 10 == 4):
-            part1 = result
+            answer = result
         opcode = program_list[index]
 
 
-    return part1, -1
+    return answer
 
-print("The answer to part 1 is %d\n"
-      "The answer to part 2 is %d" % main())
+print("The answer is %d" % main())
 
 
