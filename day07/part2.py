b60664f210a844138a31207b37732c3707a9d426
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-09 10:20:43 -0500

    Refactor day 7 to use a common "intcode computer" that can be reused

--- day07/part2.py
+++ day07/part2.py
@@ -1,6 +1,9 @@
-from opcodes import *
 from collections import deque
 from itertools import permutations
+
+import sys
+sys.path.insert(1, '..')
+from intcode.computer import IntcodeComputer
 
 def run_part2(data):
     max = 0
@@ -13,52 +16,14 @@ def run_part2(data):
     return max
 
 def single_run(data, setting):
-    running_data = [{
-        'index': 0,
-        'data': data.copy(),
-        'inputs': deque([i])}
+    running_data = [
+        IntcodeComputer(data.copy(), i)
         for i in setting]
 
     amp_pointer = 0
     last_result = 0
-    running_data[amp_pointer]['inputs'].appendleft(last_result)
-    while running_data[4]['index'] >= 0:
-        last_result = run_with_feedback(running_data[amp_pointer])
+    while running_data[4].index >= 0:
+        last_result = running_data[amp_pointer].run_with_input(last_result)
         amp_pointer = (amp_pointer + 1) % 5
-        running_data[amp_pointer]['inputs'].appendleft(last_result)
 
-    return running_data[4]['last_result']
-
-def run_with_feedback(program_data):
-    index = program_data['index']
-    program_list = program_data['data']
-    ops = [
-        None,
-        opcode_1,
-        opcode_2,
-        opcode_3,
-        opcode_4,
-        opcode_5,
-        opcode_6,
-        opcode_7,
-        opcode_8,
-    ]
-
-    opcode = program_list[index]
-    answer = -1
-    while index < len(program_list) and opcode != 99:
-        if opcode % 10 == 3:
-            num = program_data['inputs'].pop()
-            offset, _ = opcode_3(index, program_list, num)
-        else:
-            offset, result = ops[opcode % 10](index, program_list)
-        index += offset
-        if(opcode % 10 == 4):
-            program_data['index'] = index
-            program_data['last_result'] = result
-            return result
-        opcode = program_list[index]
-
-
-    program_data['index'] = -1
-    return -1
+    return running_data[4].current_result
---
+++ intcode/computer.py
@@ -0,0 +1,103 @@
+from collections import deque
+
+
+class IntcodeComputer:
+    def __init__(self, program, initial_input = None):
+        self.program = program
+        self.input_queue = deque()
+        if initial_input:
+            self.input_queue.appendleft(initial_input)
+        self.index = 0
+        self.relative_index = 0
+        self.current_result = None
+
+    def queue_input(self, num):
+        self.input_queue.appendleft(num)
+
+    def opcode_1(self, index):
+        (left, right, target) = self.parse_mode(index)
+        self.program[target] = self.program[left] + self.program[right]
+        return 4, self.program[target]
+
+    def opcode_2(self, index):
+        (left, right, target) = self.parse_mode(index)
+        self.program[target] = self.program[left] * self.program[right]
+        return 4, self.program[target]
+
+    def opcode_3(self, index):
+        num = self.input_queue.pop()
+        self.program[self.program[index + 1]] = num
+        return 2, num
+
+    def opcode_4(self, index):
+        num = self.parse_mode(index)[0]
+        return 2, self.program[num]
+
+    def opcode_5(self, index):
+        (tst, addr, _) = self.parse_mode(index)
+        if self.program[tst]:
+            return self.program[addr] - index, -1
+        else:
+            return 3, -1
+
+    def opcode_6(self, index):
+        (tst, addr, _) = self.parse_mode(index)
+        if self.program[tst]:
+            return 3, -1
+        else:
+            return self.program[addr] - index, -1
+
+    def opcode_7(self, index):
+        (left, right, target) = self.parse_mode(index)
+        if self.program[left] < self.program[right]:
+            self.program[target] = 1
+        else:
+            self.program[target] = 0
+        return 4, -1
+
+    def opcode_8(self, index):
+        (left, right, target) = self.parse_mode(index)
+        if self.program[left] == self.program[right]:
+            self.program[target] = 1
+        else:
+            self.program[target] = 0
+        return 4, -1
+
+    def parse_mode(self, index):
+        command = self.program[index]
+        modes = [(command // 10**(x+2)) % 10 for x in range(0,3)]
+        addr_len = min(3, len(self.program) - index - 1)
+        return [(index + 1 + i if modes[i] else self.program[index + 1 + i]) for i in range(0,addr_len)]
+
+    def run_with_input(self, num):
+        self.queue_input(num)
+        return self.run()
+
+    def run(self):
+        local_index = self.index
+        ops = [
+            None,
+            self.opcode_1,
+            self.opcode_2,
+            self.opcode_3,
+            self.opcode_4,
+            self.opcode_5,
+            self.opcode_6,
+            self.opcode_7,
+            self.opcode_8,
+        ]
+
+        opcode = self.program[local_index]
+        answer = -1
+        while local_index < len(self.program) and opcode != 99:
+            offset, result = ops[opcode % 10](local_index)
+            local_index += offset
+            if(opcode % 10 == 4):
+                self.index = local_index
+                self.current_result = result
+                return result
+            opcode = self.program[local_index]
+
+
+        self.index = -1
+        return -1
