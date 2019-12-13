768aaf6708b08db0fa7d54dcd9fd93aa6b7ae716
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-13 07:54:54 -0500

    Day 12, Part 2

--- day12/main.py
+++ day12/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def parse_planet(line):
     pos =  {val[0].strip(): int(val[1])
             for val in
@@ -43,13 +45,58 @@ def main():
     with open('./input') as file:
         planets = [parse_planet(i) for i in file.readlines()]
 
-    for _ in range(1000):
+    periods = {
+        'x': {
+            'target': dir_hash(planets, 'x'),
+            'period': 0
+        },
+        'y': {
+            'target': dir_hash(planets, 'y'),
+            'period': 0
+        },
+        'z': {
+            'target': dir_hash(planets, 'z'),
+            'period': 0
+        }
+    }
+    for step in range(1, 200000):
         handle_gravity(planets)
         handle_velocity(planets)
+        if step == 1000:
+            part1 = total_energy(planets)
+        track_periods(periods, planets, step)
 
-    part1 = total_energy(planets)
+    vals = [period['period'] for _, period in periods.items()]
+    print(vals)
+    part2 = reduce(lcm, vals, 1)
 
-    return part1, -1
+    return part1, part2
+
+def dir_hash(planets, dir):
+    return tuple(
+        [planets[i][per][dir]
+         for per in ['pos', 'vel']
+         for i in range(len(planets))])
+
+def track_periods(periods, planets, step):
+    for dir in ['x', 'y', 'z']:
+        node = periods[dir]
+        period = node.get('period', 0)
+
+        if period == 0:
+            node['vals'] = node.get('vals', [])
+            val_hash = dir_hash(planets, dir)
+            if val_hash == node['target']:
+                node['period'] = step
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def gcd(a, b):
+    if a == 0:
+        return b
+
+    return gcd(b % a, a)
 
 print("The answer to part 1 is %d\n"
       "The answer to part 2 is %d" % main())
