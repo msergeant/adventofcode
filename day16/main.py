72d111f3713e6d1515426de69e4a5d36e58ee573
Mike Sergeant <sergeant.mj@gmail.com>
2019-12-21 08:35:28 -0500

    Day 16, Part 2

--- day16/main.py
+++ day16/main.py
@@ -1,13 +1,35 @@
 def main():
     with open('./input') as file:
         raw = file.read()
-        nums = [int(i) for i in raw.strip()]
+        orig = [int(i) for i in raw.strip()]
 
+    nums = orig.copy()
     for _ in range(100):
         nums = [do_phase(i, nums) for i in range(len(nums))]
 
     part1 = str("".join([str(i) for i in nums[0:8]]))
-    return part1, -1
+
+
+    offset = int(raw[0:7])
+
+    nums = orig.copy()
+    part2_nums = nums[(offset % len(nums)):]
+    part2_nums.extend((10000 - offset // len(nums) - 1) * nums)
+
+    for x in range(100):
+        part2_nums = do_part2_phase(part2_nums)
+
+    part2 = str("".join([str(i) for i in part2_nums[0:8]]))
+    return part1, part2
+
+def do_part2_phase(nums):
+    result = nums.copy()
+    total = 0
+    for i in reversed(range(len(nums))):
+        total += nums[i]
+        result[i] = total % 10
+
+    return result
 
 def do_phase(pos, nums):
     pattern = get_pattern(pos + 1, len(nums))
@@ -28,4 +50,4 @@ def get_pattern(pos, min_length):
     return result[1:]
 
 print("The answer to part 1 is %s\n"
-      "The answer to part 2 is %d" % main())
+      "The answer to part 2 is %s" % main())
