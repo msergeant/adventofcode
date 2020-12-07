91bacf9724b2a31239f0c581d1f4519d14df8183
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-06 21:30:37 -0500

    Day 5, Part 2

--- 2020/day05/main.py
+++ 2020/day05/main.py
@@ -13,12 +13,21 @@ def seat_id(seat):
     row, col = parse_seat(seat)
     return row * 8 + col
 
+def missing_seats(ids):
+    lowest = min(ids)
+    highest = max(ids)
+    for i in range(lowest, highest):
+        if i not in ids:
+            return i
+
+    return 0
+
 def main():
     with open('./input') as file:
         lines = file.readlines()
         seat_ids = [seat_id(x) for x in lines]
         part_one = max(seat_ids)
-        part_two = 0
+        part_two = missing_seats(seat_ids)
 
     return part_one, part_two
 
