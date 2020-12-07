a063ec3d2b621b9e2776ab6aeaa2cf64f9b4099c
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-07 15:54:49 -0500

    Day 7, Part 1

--- 2020/day07/main.py
+++ 2020/day07/main.py
@@ -13,28 +13,45 @@ def holds_a_shiny(bag, bag_map):
     bag_map[bag]["shiny?"] = False
     return False
 
-def shiny_gold_count(lines):
+def bags_inside(color, bag_map, outer=False):
+    holds = bag_map[color]['holds']
+    if len(holds) == 0:
+        return 1
+    else:
+        total = 0 if outer else 1
+        for subcolor in holds:
+            total += holds[subcolor] * bags_inside(subcolor, bag_map)
+        return total
+
+def count_the_stuff(lines):
     bag_map = {}
     for line in lines:
         color, rest = line.split("bags ")
         color = color.strip()
-        bag_map[color] = {'holds': re.findall(r"\d+ (\w+ \w+) bag", rest)}
+        counts = re.findall(r"(\d+ \w+ \w+) bag", rest)
+        holds = {}
+        for count in counts:
+            first_space = count.find(' ')
+            num = int(count[0:first_space])
+            hold_color = count[first_space+1:]
+            holds[hold_color] = num
+
+        bag_map[color] = {'holds': holds}
+
         if "shiny gold" in bag_map[color]['holds']:
             bag_map[color]["shiny?"] = True
-
 
     shinies = 0
     for bag in bag_map:
         if holds_a_shiny(bag, bag_map):
             shinies += 1
 
-    return shinies
+    return shinies, bags_inside("shiny gold", bag_map, True)
 
 def main():
     with open('./input') as file:
         lines = file.readlines()
-        part_one = shiny_gold_count(lines)
-        part_two = 0
+        part_one, part_two = count_the_stuff(lines)
 
         return part_one, part_two
 
