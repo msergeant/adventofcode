2a33327511136757749f1a21567f67a6a9fa9984
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-08 06:28:01 -0500

    Day 8, Part 2

--- 2020/day08/main.py
+++ 2020/day08/main.py
@@ -1,11 +1,11 @@
-def first_repeated_instruction(instructions):
+def run_instructions(instructions):
     acc = 0
     inst = 0
     visited = []
 
     while inst < len(instructions):
         if inst in visited:
-            return acc
+            return False, acc, visited
 
         visited.append(inst)
 
@@ -21,14 +21,32 @@ def first_repeated_instruction(instructions):
         if oper == "acc":
             acc += amount
 
-    return 0
+    return True, acc, visited
 
+def swap_instruction(instruction):
+    if instruction[0:3] == "nop":
+        return instruction.replace("nop", "jmp")
+    else:
+        return instruction.replace("jmp", "nop")
+
+def find_bad_line(instructions):
+    _, _, inst_list = run_instructions(instructions)
+
+    for index in inst_list:
+        inst = instructions[index]
+        if not inst[0:3] == "acc":
+            instructions[index] = swap_instruction(inst)
+            success, acc, _ = run_instructions(instructions)
+            if success:
+                return acc
+
+            instructions[index] = inst
 
 def main():
     with open('./input') as file:
         lines = file.readlines()
-        part_one = first_repeated_instruction(lines)
-        part_two = 0
+        _, part_one, _ = run_instructions(lines)
+        part_two = find_bad_line(lines)
 
     return part_one, part_two
 
