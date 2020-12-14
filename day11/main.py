393c16e1a3dafaace55f205c41c99ea229c92f64
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-13 22:44:23 -0500

    Day 11, Part 2

--- day11/main.py
+++ day11/main.py
@@ -2,29 +2,45 @@ FLOOR = 0
 OPEN = 1
 OCCUPIED = 2
 
+DIRECTIONS = (
+    (-1, -1),
+    (-1,  0),
+    (-1,  1),
+    ( 0,  1),
+    ( 1,  1),
+    ( 1,  0),
+    ( 1, -1),
+    ( 0, -1),
+)
+
 def count_adjacent_taken(x, y, height, width, grid):
     total = 0
-    if x > 0 and grid[y][x-1] == OCCUPIED:
-        total += 1
-    if x < width and grid[y][x+1] == OCCUPIED:
-        total += 1
-    if y > 0 and grid[y - 1][x] == OCCUPIED:
-        total += 1
-    if y < height and grid[y + 1][x] == OCCUPIED:
-        total += 1
-
-    if y > 0 and x > 0 and grid[y - 1][x - 1] == OCCUPIED:
-        total += 1
-    if y > 0 and x < width and grid[y - 1][x + 1] == OCCUPIED:
-        total += 1
-    if y < height and x < width and grid[y + 1][x + 1] == OCCUPIED:
-        total += 1
-    if y < height and x > 0 and grid[y + 1][x - 1] == OCCUPIED:
-        total += 1
+    for i, j in DIRECTIONS:
+        ex = x + i
+        ey = y + j
+        if (ex >= 0 and ey >= 0 and ex < width and ey < height and
+           grid[ey][ex] == OCCUPIED):
+            total += 1
 
     return total
 
-def occupied_after_stabilization(lines):
+def count_line_of_sight(x, y, height, width, grid):
+    total = 0
+    for i, j in DIRECTIONS:
+        ex = x + i
+        ey = y + j
+        while (ex >= 0 and ey >= 0 and ex < width and ey < height):
+            if grid[ey][ex] == OCCUPIED:
+                total += 1
+                break
+            elif grid[ey][ex] == OPEN:
+                break
+            ex += i
+            ey += j
+
+    return total
+
+def occupied_after_stabilization(lines, adj_function, target):
     current_grid = []
 
     for line in lines:
@@ -37,25 +53,24 @@ def occupied_after_stabilization(lines):
 
         current_grid.append(row)
 
-    height = len(lines) - 1
-    width  = len(current_grid[0]) - 1
+    height = len(current_grid)
+    width  = len(current_grid[0])
 
     while True:
         new_grid = []
         occupied_seats = 0
         changed_row = False
-        for y in range(len(lines)):
-            row = current_grid[y]
+        for y, row in enumerate(current_grid):
             new_row = []
             for x, spot in enumerate(row):
                 if spot == FLOOR:
                     new_row.append(FLOOR)
                 else:
-                    adjacent = count_adjacent_taken(x, y,
-                                                    height, width,
-                                                    current_grid)
+                    adjacent = adj_function(x, y,
+                                            height, width,
+                                            current_grid)
 
-                    if ((spot == OCCUPIED and adjacent < 4) or
+                    if ((spot == OCCUPIED and adjacent < target) or
                        (spot == OPEN and adjacent == 0)):
                         occupied_seats += 1
                         new_row.append(OCCUPIED)
@@ -77,8 +92,8 @@ def occupied_after_stabilization(lines):
 def main():
     with open('./input') as file:
         lines = file.readlines()
-        part_one = occupied_after_stabilization(lines)
-        part_two = 0
+        part_one = occupied_after_stabilization(lines, count_adjacent_taken, 4)
+        part_two = occupied_after_stabilization(lines, count_line_of_sight, 5)
 
     return part_one, part_two
 
