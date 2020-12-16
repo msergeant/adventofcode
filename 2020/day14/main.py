f9d82525a102dbf6893e924f9157cce4db4bcc51
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-16 11:42:53 -0500

    Day 14, Part 2

--- 2020/day14/main.py
+++ 2020/day14/main.py
@@ -15,11 +15,31 @@ def read_mask(mask):
 
     return int(or_str, 2), int(and_str, 2)
 
+def addresses(mask):
+    if 'X' in mask:
+        return (addresses(re.sub('X', '1', mask, 1)) +
+                addresses(re.sub('X', '0', mask, 1)))
+    else:
+        return [int(mask, 2)]
+
+def find_floats(addr, mask):
+    new_mask = ''
+    for i, chr in enumerate(mask):
+        if chr == 'X':
+            new_mask += chr
+        elif chr == '1' or (addr & (2 ** (35 - i))):
+            new_mask += '1'
+        else:
+            new_mask += '0'
+
+    return(addresses(new_mask))
+
 
 def process_input(lines):
     index = 0
 
     mem = {}
+    mem2 = {}
     while index < len(lines):
         if lines[index].startswith("mask"):
             mask = lines[index].split('= ')[1]
@@ -29,20 +49,26 @@ def process_input(lines):
             while index < len(lines) and not lines[index].startswith("mask"):
                 addr = int(lines[index].split(']')[0][4:])
                 val = int(lines[index].split('=')[1])
+                val2 = val
 
                 val = val | or_val
                 val = val & and_val
 
                 mem[addr] = val
+
+                # part 2
+                addrs = find_floats(addr, mask)
+                for a in addrs:
+                    mem2[a] = val2
+
                 index += 1
 
-    return sum(mem.values())
+    return sum(mem.values()), sum(mem2.values())
 
 def main():
     with open('./input') as file:
         lines = [x.strip() for x in file.readlines()]
-        part_one = process_input(lines)
-        part_two = 0
+        part_one, part_two = process_input(lines)
 
     return part_one, part_two
 
