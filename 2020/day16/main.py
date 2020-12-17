df6bfaf814664d3588fe4fde2a000339bd514dca
Mike Sergeant <sergeant.mj@gmail.com>
2020-12-16 22:52:14 -0500

    Day 16, Part 2

--- 2020/day16/main.py
+++ 2020/day16/main.py
@@ -1,15 +1,48 @@
-def sum_invalid_tickets(lines):
+def find_index(num, data):
+    every_index_found = True
+    for key in data:
+        if num in data[key]['valid_indices']:
+            data[key]['valid_indices'].remove(num)
+
+        if not data[key].get('index'):
+            every_index_found = False
+
+    if every_index_found:
+        return None
+
+    for key in data:
+        if len(data[key]['valid_indices']) == 1:
+            index = list(data[key]['valid_indices'])[0]
+            data[key]['index'] = index
+            return find_index(index, data)
+
+def process_ticket_info(lines):
     valid_nums = [False for x in range(1000)]
-    range_strs = []
     index = 0
+
+    data = {}
     while lines[index]:
-        range_strs.extend(lines[index].split(':')[1].split('or'))
+        key = lines[index].split(':')[0].strip()
+        range_strs = lines[index].split(':')[1].split('or')
+        ranges = []
+        for range_str in range_strs:
+            bot, top = [int(x.strip()) for x in range_str.split('-')]
+            ranges.extend([bot, top])
+            for i in range(bot, top + 1):
+                valid_nums[i] = True
+
+        data[key] = {
+            'ranges': ranges,
+            'valid_indices': set(range(20))
+        }
         index += 1
 
-    for range_str in range_strs:
-        bot, top = [int(x.strip()) for x in range_str.split('-')]
-        for i in range(bot, top + 1):
-            valid_nums[i] = True
+    while lines[index] != "your ticket:":
+        index += 1
+
+    index += 1
+
+    your_ticket = [int(x.strip()) for x in lines[index].split(',')]
 
     while lines[index] != "nearby tickets:":
         index += 1
@@ -18,19 +51,35 @@ def sum_invalid_tickets(lines):
 
     invalid_total = 0
     while index < len(lines):
+        valid_ticket = True
         for i in [int(x) for x in lines[index].split(',')]:
             if not valid_nums[i]:
+                valid_ticket = False
                 invalid_total += i
+
+        if valid_ticket:
+            for i, num in enumerate([int(x) for x in lines[index].split(',')]):
+                for key in data:
+                    ranges = data[key]['ranges']
+                    if num < ranges[0] or (num > ranges[1] and num < ranges[2]):
+                        if i in data[key]['valid_indices']:
+                            data[key]['valid_indices'].remove(i)
 
         index += 1
 
-    return invalid_total
+    find_index(-1, data)
+
+    product = 1
+    for key in data:
+        if "departure" in key:
+            product *= your_ticket[data[key]['index']]
+
+    return invalid_total, product
 
 def main():
     with open('./input') as file:
         lines = [x.strip() for x in file.readlines()]
-        part_one = sum_invalid_tickets(lines)
-        part_two = 0
+        part_one, part_two = process_ticket_info(lines)
 
     return part_one, part_two
 
