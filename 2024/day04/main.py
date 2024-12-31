2441803fa6fe958dcd6d6299f6ab3ef82245ffea
Mike Sergeant <sergeant.mj@gmail.com>
2024-12-30 20:04:19 -0500

    Day 4, Part 2

--- 2024/day04/main.py
+++ 2024/day04/main.py
@@ -26,6 +26,21 @@ def is_xmas(line_num, char_num, lines):
     return total
 
 
+def is_cross_xmas(line_num, char_num, lines):
+    if not (lines[line_num][char_num] == "A" and
+            line_num > 0 and
+            line_num < len(lines) - 1 and
+            char_num > 0 and
+            char_num < len(lines[0]) - 1):
+        return False
+
+    backslash = set([lines[line_num - 1][char_num - 1], lines[line_num + 1][char_num + 1]])
+    forwardslash = set([lines[line_num - 1][char_num + 1], lines[line_num + 1][char_num - 1]])
+
+    return (backslash == {"M", "S"} and
+            forwardslash == {"M", "S"})
+
+
 def main():
     lines = []
     with open("input") as file:
@@ -33,11 +48,16 @@ def main():
             lines.append(line.strip())
 
     total = 0
+    total2 = 0
     for line in range(len(lines)):
         for char in range(len(lines[0])):
             total += is_xmas(line, char, lines)
 
+            if is_cross_xmas(line, char, lines):
+                total2 += 1
+
     print(f"The answer to part one is {total}")
+    print(f"The answer to part two is {total2}")
 
 
 main()
