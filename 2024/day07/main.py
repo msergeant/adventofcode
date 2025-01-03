8d1404174c55fd01c38b90edd433a05aa7167b85
Mike Sergeant <sergeant.mj@gmail.com>
2025-01-02 23:18:46 -0500

    Day 7, Part 2

--- 2024/day07/main.py
+++ 2024/day07/main.py
@@ -9,11 +9,16 @@ def times(x, y):
     return x * y
 
 
+def joiner(x, y):
+    return int(str(x) + str(y))
+
+
 OPERS = [plus, times]
+PART2_OPERS = [plus, times, joiner]
 
 
-def process_line(goal, args):
-    for oper in product(OPERS, repeat=len(args) - 1):
+def process_line(goal, args, operators=OPERS):
+    for oper in product(operators, repeat=len(args) - 1):
         acc = args[0]
         for i, arg in enumerate(args[1:]):
             acc = oper[i](acc, arg)
@@ -26,14 +31,18 @@ def process_line(goal, args):
 
 def main():
     total = 0
+    total2 = 0
     with open("input") as file:
         for line in file.readlines():
             goal, args = line.split(":")
             nums = [int(x) for x in args.strip().split(" ")]
             if process_line(int(goal), nums):
                 total += int(goal)
+            if process_line(int(goal), nums, PART2_OPERS):
+                total2 += int(goal)
 
     print(f"The answer to part one is {total}")
+    print(f"The answer to part two is {total2}")
 
 
 main()
