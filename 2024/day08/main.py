50a3b81439e55ee28e2bbdbaeadd81303694aba3
Mike Sergeant <sergeant.mj@gmail.com>
2025-01-05 18:00:39 -0500

    Day 8, Part 2

--- 2024/day08/main.py
+++ 2024/day08/main.py
@@ -25,21 +25,41 @@ def main():
                     antennas[char].append((j, i))
 
     unique_antinodes = set()
+    unique_antinodes_many = set()
     for char in antennas:
         for node1, node2 in combinations(antennas[char], 2):
             y_diff = abs(node1[1] - node2[1])
             x_diff = abs(node1[0] - node2[0])
 
-            antinode1 = (node1[0] + sign_of(node1[0], node2[0]) * x_diff,
-                         node1[1] + sign_of(node1[1], node2[1]) * y_diff)
-            antinode2 = (node2[0] + sign_of(node2[0], node1[0]) * x_diff,
-                         node2[1] + sign_of(node2[1], node1[1]) * y_diff)
-
-            for x, y in [antinode1, antinode2]:
-                if x >= 0 and x < len(lines[0]) and y >= 0 and y < len(lines):
+            unique_antinodes_many.add(node1)
+            direction = (sign_of(node1[0], node2[0]) * x_diff,
+                         sign_of(node1[1], node2[1]) * y_diff)
+            x = node1[0] + direction[0]
+            y = node1[1] + direction[1]
+            count = 0
+            while (x >= 0 and x < len(lines[0]) and y >= 0 and y < len(lines)):
+                if count == 0:
                     unique_antinodes.add((x, y))
+                unique_antinodes_many.add((x, y))
+                x += direction[0]
+                y += direction[1]
+                count += 1
+
+            unique_antinodes_many.add(node2)
+            direction = (-direction[0], -direction[1])
+            x = node2[0] + direction[0]
+            y = node2[1] + direction[1]
+            count = 0
+            while (x >= 0 and x < len(lines[0]) and y >= 0 and y < len(lines)):
+                if count == 0:
+                    unique_antinodes.add((x, y))
+                unique_antinodes_many.add((x, y))
+                x += direction[0]
+                y += direction[1]
+                count += 1
 
     print(f"The answer to part one is {len(unique_antinodes)}")
+    print(f"The answer to part two is {len(unique_antinodes_many)}")
 
 
 main()
